a62c92938bfaa0f5d9f6beb2bd6b35aeb44839a4
George Kochera <george.kochera@gmail.com>
2021-04-14 20:36:44 -0400

    able to return coordinates of bounding boxes now

--- yolotest.py
+++ yolotest.py
@@ -2,7 +2,32 @@
 yolotest.py
 """
 import torch
-from . import yolov3
-print(yolov3.debug)
+import numpy as np
 
-#imgs = ['./input/verify/2018-audi-a4.jpg']
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Images
+dir = './'
+imgs = [dir + f for f in ('cars.jpg',)]  # batched list of images
+
+# Inference
+results = model(imgs)
+
+# Results
+# results.print()  
+# results.save()  # or .show()
+
+# Data Processing Section
+
+# Extract the coordinates of the bounding box from the YOLOv5 results
+rough_coordinates = list(results.xyxy[0].cpu().numpy())
+
+# Create a list of coordinate lists we can pass to the Photo Cropper
+coordinate_list = []
+for row in rough_coordinates:
+    row = list(row)
+    row = row[:4]
+    coordinate_list.append(row)
+
+print(coordinate_list)

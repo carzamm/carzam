4b43def1c778d444b56ea053cd087cb345415c70
George Kochera <george.kochera@gmail.com>
2021-04-23 18:09:18 -0400

    script sucessfully ids and crops photos with more than 1 car now

--- yolotest.py
+++ yolotest.py
@@ -14,6 +14,9 @@ import os
 # Model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
+# Set YOLO to only detect cars (id = 2) and trucks (id = 7)
+model.classes = [2, 7]
+
 # Images
 indir = './data/testimages/'
 outdir = './data/outimages/'
@@ -24,27 +27,34 @@ crop_instructions = [] # list of 5-tuples (filename, x-left, y-top, x-right, y-b
 for img in imgs:
 
     # Inference
+    # results will be a list of Detection objects (cars, trucks, etc.)
     results = model(img).tolist()
 
-    # Clean up the results by converting from Tensor to List
-    for result in results:
+    # For each detection object, go through all the objects detected for that class of detection
+    for detection in results:
+        
+        # Print the relevant filename
+        print("\nFile ---> {}".format(img))
 
         # Bring the 'xyxy' tensors off the GPU back to the CPU and convert to a list
-        location = result.xyxy.cpu().numpy().tolist()
+        vehicles = detection.xyxy.cpu().numpy().tolist()
 
-        # Set some variables to track the pixel coordinates
-        x_left = int(location[0][0])
-        y_top = int(location[0][1])
-        x_right = int(location[0][2])
-        y_bottom = int(location[0][3])
+        # For vehicle detected in the detection object, get the relevant information
+        for index, vehicle in enumerate(vehicles):
 
-        # Output the finding to the console
-        print("\nFile ---> {}".format(img))
-        print("\tTop-Left (x,y): {},{}".format(x_left, y_top))
-        print("\tBottom-Right (x,y): {}, {}".format(x_right, y_bottom))
+            # Set some variables to track the pixel coordinates
+            x_left = int(vehicle[0])
+            y_top = int(vehicle[1])
+            x_right = int(vehicle[2])
+            y_bottom = int(vehicle[3])
 
-        # Add this to the crop instructions so the image cropper can generate the new pictures
-        crop_instructions.append((str(img), x_left, y_top, x_right, y_bottom))
+            # Output the finding to the console
+            print("\n #{}".format(index + 1))
+            print("\tTop-Left (x,y): {},{}".format(x_left, y_top))
+            print("\tBottom-Right (x,y): {}, {}".format(x_right, y_bottom))
+
+            # Add this to the crop instructions so the image cropper can generate the new pictures
+            crop_instructions.append((str(img), x_left, y_top, x_right, y_bottom))
 
 #
 #   IMAGE CROPPER SECTION ---------------------------------------------------------------------

2442c2aeb447e7a28c2a65c4d926925cac3bb60d
Kevin Joy <joykm88@gmail.com>
2021-05-09 14:05:37 -0400

    dynamic input text

--- static/js/index.js
+++ static/js/index.js
@@ -1,31 +1,47 @@
 function preloader(){
     document.getElementById("loading").style.display = "none";
     document.getElementById("content").style.display = "block";
-}//preloader
-window.onload = preloader;
-
-console.log("test");
-
-// get the original input element which is hidden
-var imageName = document.getElementById('choose-file');
-console.log(imageName);
-
-// fuction to change label text
-function editLabel() {
-	console.log("test");
-	// gets the filepath of the user chosen file from input element
-    var filePath = imageName.value; 
-
-    // gets the file name from file path
-    var fileNameStart = filePath.lastIndexOf('\\');
-    filePath = filePath.substr(fileNameStart + 1);
-
-    // change the label text
-    var newLabelText = document.querySelector('label[for="choose-file"]').childNodes[2]; /* finds the label text */
-    if (filePath !== '') {
-        newLabelText.textContent = filePath;
-    }
 }
+
+// get input element once dom content loads
+function domLoaded() {
+
+	// get the original input element which is hidden
+	var imageName = document.getElementById('choose-file');
+
+	// fuction to change label text
+	function editLabel() {
+		// gets the filepath of the user chosen file from input element
+	    var filePath = imageName.value; 
+
+	    // gets the file name from file path
+	    var fileNameStart = filePath.lastIndexOf('\\');
+	    filePath = filePath.substr(fileNameStart + 1);
+
+	    // select the label element
+	    var newLabelText = document.querySelector('label[for="choose-file"]');
+
+	    // iterate the children of the label element and only change the text
+		for(const node of newLabelText.childNodes) {
+			if(node.nodeName === '#text') {
+				if (filePath !== '') {
+	        		node.textContent = filePath;
+	    		}
+			}
+		}
+	}
 
 // event listener to detect file a new file choise and edit the label text
 imageName.addEventListener('change',editLabel,false);
+
+}
+
+// call preloader when window loads
+window.onload = preloader;
+
+// call domLoaded when dom content is loaded
+document.addEventListener('DOMContentLoaded', domLoaded, false);
+
+
+
+
